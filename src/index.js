58cc3e2018ad8d6b9d04dfe2b6caf050a0ebf6ad
Nadia-si <nasimbi2011@gmail.com>
2023-04-05 00:06:42 +0200

    Adjusted real time data for cities

--- src/index.js
+++ src/index.js
@@ -1,36 +1,95 @@
-function displayTime() {
-  var refresh = 1000; // Refresh rate in milli seconds
-  mytime = setTimeout("checkSeconds()", refresh);
-}
-displayTime();
+function currentTime() {
+  let time = new Date();
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+  let seconds = time.getSeconds();
 
-let currentTime = new Date();
-function checkHour() {
-  let hours = currentTime.getHours();
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
   if (hours < 10) {
     hours = "0" + hours;
   }
   newformat = hours >= 12 ? "PM" : "AM";
   hours = hours % 12;
   hours = hours ? hours : 12;
-  return hours;
-}
-function checkMinutes() {
-  let minutes = currentTime.getMinutes();
-  if (minutes < 10) {
-    minutes = "0" + minutes;
-  }
-  return minutes;
-}
-function checkSeconds() {
-  let seconds = currentTime.getSeconds();
-  if (seconds < 10) {
-    seconds = `0${seconds}`;
-  }
-  return seconds;
-}
 
-let todayTime = document.querySelector("#realTime");
-let timeNow = document.querySelector("#timeNow");
-todayTime.innerHTML = ` ${checkHour()}:${checkMinutes()}:${checkSeconds()} ${newformat}`;
-timeNow.innerHTML = ` ${checkHour()}:${checkMinutes()}:${checkSeconds()}`;
+  let realTime = hours + ":" + minutes + ":" + seconds + " " + newformat;
+  let timeToronto = document.querySelector("#timeToronto");
+  let timeKigali = document.querySelector("#timeKigali");
+  let timeCape = document.querySelector("#timeCape");
+  let timeSydney = document.querySelector("#timeSydney");
+  timeToronto.innerHTML = time.toLocaleTimeString("en-US", {
+    timeZone: "America/Toronto",
+  });
+  timeSydney.innerHTML = time.toLocaleTimeString(undefined, {
+    timeZone: "Australia/Sydney",
+  });
+  timeKigali.innerHTML = realTime;
+  timeCape.innerHTML = realTime;
+
+  let t = setTimeout(function () {
+    currentTime();
+  }, 1000);
+}
+currentTime();
+
+let time = new Date();
+let day = time.getDate();
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[time.getMonth()];
+let year = time.getFullYear();
+let currentDate = `${month} ${day}th ${year}`;
+
+let dateToronto = document.querySelector(".dateToronto");
+dateToronto.innerHTML = currentDate;
+let dateKigali = document.querySelector(".dateKigali");
+dateKigali.innerHTML = currentDate;
+let dateCape = document.querySelector(".dateCape");
+dateCape.innerHTML = currentDate;
+let dateSydney = document.querySelector(".dateSydney");
+let options = {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+};
+dateSydney.innerHTML = time.toLocaleDateString("en-US", {
+  month: "long",
+  year: "numeric",
+  day: "numeric",
+});
+//dateSydney.innerHTML = month[sydneyDate];
+
+/*
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+const d = new Date();
+document.write("The current month is " + monthNames[d.getMonth()]);
+
+let year = date.getFullYear();
+let day = date.getDate();
+
+let currentDate = `${day}-${month}-${year}`;
+
+*/
