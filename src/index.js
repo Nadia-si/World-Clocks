390822fe7a44ce7f271a62a96ffd9cc607041238
Nadia-si <nasimbi2011@gmail.com>
2023-07-23 00:31:20 +0200

    Added current location

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ function currentTime() {
   let hours = time.getHours();
   let minutes = time.getMinutes();
   let seconds = time.getSeconds();
-
   if (minutes < 10) {
     minutes = "0" + minutes;
   }
@@ -20,67 +19,87 @@ function currentTime() {
   hours = hours ? hours : 12;
 
   let realTime = hours + ":" + minutes + ":" + seconds + " " + newformat;
+
+  let day = time.getDate();
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[time.getMonth()];
+  let year = time.getFullYear();
+  let currentDate = `${month} ${day}, ${year}`;
+
+  //Toronto
   let timeToronto = document.querySelector("#timeToronto");
-  let timeKigali = document.querySelector("#timeKigali");
-  let timeCape = document.querySelector("#timeCape");
-  let timeSydney = document.querySelector("#timeSydney");
   timeToronto.innerHTML = time.toLocaleTimeString("en-US", {
     timeZone: "America/Toronto",
   });
+  let dateToronto = document.querySelector("#dateToronto");
+  dateToronto.innerHTML = time.toLocaleDateString("en-US", {
+    timeZone: "America/Toronto",
+    month: "long",
+    year: "numeric",
+    day: "numeric",
+  });
+  //Sydney
+  let timeSydney = document.querySelector("#timeSydney");
   timeSydney.innerHTML = time.toLocaleTimeString(undefined, {
     timeZone: "Australia/Sydney",
   });
+  let dateSydney = document.querySelector("#dateSydney");
+  dateSydney.innerHTML = time.toLocaleDateString("en-US", {
+    timeZone: "Australia/Sydney",
+    month: "long",
+    year: "numeric",
+    day: "numeric",
+  });
+  //Kigali
+  let timeKigali = document.querySelector("#timeKigali");
   timeKigali.innerHTML = realTime;
+  let dateKigali = document.querySelector("#dateKigali");
+  dateKigali.innerHTML = currentDate;
+  //Cape Town
+  let timeCape = document.querySelector("#timeCape");
   timeCape.innerHTML = realTime;
+  let dateCape = document.querySelector("#dateCape");
+  dateCape.innerHTML = currentDate;
 
   let t = setTimeout(function () {
     currentTime();
   }, 1000);
+  function search(event) {
+    let cities = document.querySelector("#cities");
+    let value = event.target.value;
+    if (value === "current") {
+      value = moment.tz.guess();
+    }
+    if (value.length) {
+      let searchTime = moment().tz(value);
+      let city = value.split("/")[1].replace("_", " ");
+      let time = searchTime.format("h:mm:ss A");
+      let currentDate = searchTime.format("MMMM Do YYYY");
+      cities.innerHTML = `
+		<div class="city">			
+				<h2>${city}
+        <div class="date">${currentDate}</div>
+        </h2>
+			<div class="time">${time}</div>
+		</div>
+		`;
+    }
+  }
+
+  let select = document.getElementById("search");
+  select.addEventListener("change", search);
 }
 currentTime();
-
-let time = new Date();
-let day = time.getDate();
-let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-let month = months[time.getMonth()];
-let year = time.getFullYear();
-let currentDate = `${month} ${day}, ${year}`;
-
-let dateToronto = document.querySelector(".dateToronto");
-
-dateToronto.innerHTML = time.toLocaleDateString("en-US", {
-  timeZone: "America/Toronto",
-  month: "long",
-  year: "numeric",
-  day: "numeric",
-});
-let dateKigali = document.querySelector(".dateKigali");
-dateKigali.innerHTML = currentDate;
-let dateCape = document.querySelector(".dateCape");
-dateCape.innerHTML = currentDate;
-let dateSydney = document.querySelector(".dateSydney");
-dateSydney.innerHTML = time.toLocaleDateString("en-US", {
-  timeZone: "Australia/Sydney",
-  month: "long",
-  year: "numeric",
-  day: "numeric",
-});
-
-let selectLocation = document.querySelector("#search");
-selectLocation.addEventListener("change", (event) => {
-  let cities = document.querySelector("#cities");
-  cities.innerHTML = `${event.target.value}`;
-});
